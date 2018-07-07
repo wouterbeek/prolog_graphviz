ea2c5e830855066191277c11b5f49890171a599f
Wouter Beek <wouter@triply.cc>
2018-07-07 12:55:16 +0200

    DEPLOYMENT: Prolog Pack → PPM.

--- pack.pl
+++
@@ -1,5 +0,0 @@
-home('https://github.com/wouterbeek/graph_export').
-keywords(['DOT',export,graph,'GraphViz']).
-name(graph_export).
-title('A Prolog library for exporting graphs using the DOT language.').
-version('0.0.1').

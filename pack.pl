c64fdf9b1d1d80b94bb8a52d81147e0c99e6e8eb
Wouter Beek <wouter@triply.cc>
2020-05-15 20:14:40 +0200

    1.0.2

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 author('Wouter Beek', 'wouter@triply.cc').
-dependency(prolog_library_collection).
 download('https://github.com/wouterbeek/prolog_graphviz/releases/*.zip').
 home('https://github.com/wouterbeek/prolog_graphviz').
 maintainer('Wouter Beek', 'wouter@triply.cc').
 name(prolog_graphviz).
 packager('Wouter Beek', 'wouter@triply.cc').
+requires(prolog_library_collection).
 title('Prolog Graph Visualization').
-version('1.0.1').
+version('1.0.2').
